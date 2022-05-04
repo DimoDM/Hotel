d1743aac4ab2d782ed7b18795e2d0952d677fd8c
DimoDM <dimo_marino@abv.bg>
2022-05-04 20:17:50 +0300

    struct for Vector2

--- Vector.h
+++ Vector.h
@@ -1,1 +1,24 @@
 #pragma once
+
+template<typename T>
+class Vector
+{
+private:
+	size_t capacity;
+	size_t size;
+	T* data;
+
+	void copyFrom(const T&);
+	void free();
+
+public:
+	Vector();
+	Vector(const T&);
+	const T& operator=(const T&);
+	T& operator[](int);
+
+	void push_back(const T&);
+
+	~Vector();
+
+};
