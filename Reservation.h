f56552c0541c348084187c78a98d64ca02424cd6
DimoDM <dimo_marino@abv.bg>
2022-05-05 00:54:26 +0300

    reservation base

--- Reservation.h
+++ Reservation.h
@@ -1,1 +1,17 @@
 #pragma once
+#include"Room.h"
+#include"Date.h"
+class Reservation : Room
+{
+	char* name;
+	Interval interval;
+
+public:
+
+	Reservation();
+	void setName(const char*);
+	void setInterval(const Interval&);
+	bool operator==(const Reservation&);
+
+	~Reservation();
+};
